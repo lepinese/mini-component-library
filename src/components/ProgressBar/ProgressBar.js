1e4f268201d736d808923d3084047c1aa820a936
Sébastien Lépine <sebastien.lepine@radio-canada.ca>
2022-06-09 10:32:56 -0400

    Progress bar and Select

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,62 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = sizes[size];
+  return (
+    <ProgressBarWrapper style={styles} role="progressbar" aria-valuenow={value}>
+      <ProgressBarPadding>
+        <Progression style={styles} value={value} />
+        <VisuallyHidden>Progression is at {value}%</VisuallyHidden>
+      </ProgressBarPadding>
+    </ProgressBarWrapper>
+  );
 };
+
+const sizes = {
+  small: {
+    "--outer-radius": "4px",
+    "--inner-radius": "4px",
+    "--height": "8px",
+    "--padding": "0px",
+  },
+  medium: {
+    "--outer-radius": "4px",
+    "--inner-radius": "4px",
+    "--height": "12px",
+    "--padding": "0px",
+  },
+  large: {
+    "--outer-radius": "8px",
+    "--inner-radius": "4px",
+    "--height": "16px",
+    "--padding": "4px",
+  },
+};
+
+const ProgressBarWrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border-radius: var(--outer-radius);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: var(--padding);
+  width: 370px;
+`;
+
+const ProgressBarPadding = styled.div`
+  border-radius: var(--inner-radius);
+  overflow: hidden;
+  width: 100%;
+`;
+
+const Progression = styled.span`
+  background-color: ${COLORS.primary};
+  display: block;
+  height: var(--height);
+  width: ${(props) => props.value}%;
+`;
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,68 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <VisuallyHidden>label</VisuallyHidden>
+      <TrueSelect value={value} onChange={onChange}>
+        {children}
+      </TrueSelect>
+      <FakeSelect aria-hidden>
+        {displayedValue}
+        <ChevronIcon id="chevron-down" size="24" strokeWidth="2" />
+      </FakeSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  color: ${COLORS.gray700};
+  position: relative;
+  width: fit-content;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const TrueSelect = styled.select`
+  appearance: none;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  font-size: 0; // hides the text for the current selection
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+`;
+
+const FakeSelect = styled.div`
+  align-items: center;
+  background: ${COLORS.transparentGray15};
+  border: none;
+  border-radius: 8px;
+  display: flex;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  padding: 12px 10px 12px 16px;
+  pointer-events: none;
+  position: relative;
+  width: fit-content;
+`;
+
+const ChevronIcon = styled(Icon)`
+  margin-left: 24px;
+`;
 
 export default Select;
